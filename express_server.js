7050837629359e40ac759bc4dd77a23cc9e909e1
Joacquim Mukadi <joacquimm@gmail.com>
2021-04-12 23:24:55 +0000

    made slight modifications

--- express_server.js
+++ express_server.js
@@ -1,8 +1,16 @@
+
+//----------------------------------------------------------------
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+const generateRandomString = require("./helper.js");
+// -- ejs --
 app.set("view engine", "ejs");
+
+//-- body parser --
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 // -- URL Database -- 
 const urlDatabase = {
@@ -24,16 +32,36 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+// -- urls page -- 
 app.get("/urls", (req, res) =>{
   const templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
 })
+// -- new url page --
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
+// -- short url page --
 app.get("/urls/:shortURL", (req, res) =>{
   console.log(" REQ PARAMS -->",req.params);
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 })
+
+
+// -- POST ROUTES --
+
+app.post("/urls", (req, res) => {
+  console.log(" REQ BODY -->",req.body);  // Log the POST request body to the console
+  console.log(urlDatabase);
+  const userLongUrl = req.body.longURL;
+  const userShortUrl = generateRandomString();
+  urlDatabase[userShortUrl] = userLongUrl;
+  res.redirect(`/urls/${userShortUrl}`); 
+  
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
